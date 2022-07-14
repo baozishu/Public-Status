c7cfb17fe4e7c93e3c5d604ff8e0e9b06951fea8
包子叔 <60163088+baozishu@users.noreply.github.com>
2022-07-14 22:24:58 +0800

    更新

--- config.js
+++ config.js
@@ -1,35 +1,30 @@
-// 配置
 window.Config = {
 
-  // 站点名
-  SiteName: 'Uptime Status',
-
-  // 站点链接
-  SiteUrl: '/',
+  // 显示标题
+  SiteName: 'Site status',
 
   // UptimeRobot Api Keys
-  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
+  // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'ur886978-da6e06b8f1a401757b0893bf',
+
   ],
 
-  // 是否显示监测站点的链接
-  ShowLink: true,
-
   // 日志天数
-  // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
-  // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 30,
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
 
   // 导航栏菜单
   Navi: [
     {
-      text: '353.ee',
-      url: 'https://353.ee/'
+      text: 'Team',
+      url: 'https://team.353.ee/'
     },
     {
       text: 'Status',
       url: '#'
-    }
-  ]
+    },
+  ],
 };
